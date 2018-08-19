3aba5b45b4286ad7f5207d10447c6e64f7c3668a
tofuchen <tofuchen@126.com>
2018-08-19 10:34:20 +0800

    删除主函数（test）

--- src/com/nowcoder/problem13/Solution.java
+++ src/com/nowcoder/problem13/Solution.java
@@ -9,13 +9,6 @@ import java.util.Arrays;
  *
  */
 public class Solution {
-	
-	public static void main(String[] args) {
-		int[] array= {1,2,3,4,5,6,7};
-		System.out.println(Arrays.toString(array));
-		new Solution().reOrderArray(array);
-		System.out.println(Arrays.toString(array));
-	}
 
 	public void reOrderArray(int[] array) {
 		int index=0;
